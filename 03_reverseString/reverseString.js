9c2e9526271262cc035225ada67db0c84db53ea8
nicolaslouge <53574879+nicolaslouge@users.noreply.github.com>
2024-09-21 23:20:54 -0400

    Completed ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringTab = string.split('');
+    let reversedString = "";
+    for(let i=0; i < stringTab.length;i++) {
+        reversedString += stringTab[stringTab.length-1-i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
