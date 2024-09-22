c9d26a6d358981d14817d13862c0715ef88b542b
nicolaslouge <53574879+nicolaslouge@users.noreply.github.com>
2024-09-22 19:49:00 -0400

    Completed ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedValues) {
+  const newArray = [];
+  array.forEach((item) => {
+    if (!removedValues.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
