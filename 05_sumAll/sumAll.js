0c0a6db4db77222aa11bdddd1e4d58d259358f9d
nicolaslouge <53574879+nicolaslouge@users.noreply.github.com>
2024-09-22 20:31:27 -0400

    Completed ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    if (!Number.isInteger(intA) || !Number.isInteger(intB)) return "ERROR";
+    if (intA < 0 || intB < 0) return "ERROR";
+    let sum =0;
+    if(intB>intA) {
+        do {
+            sum += intB;
+            intB--; 
+        } while(intB>=intA)
+    } else {
+        do {
+            sum += intA;
+            intA--; 
+        } while(intA>=intB)
+    }
+    return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
