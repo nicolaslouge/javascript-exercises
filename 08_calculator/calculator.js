47850b84af2e75952635f4393bba0142bbdccf2a
nicolaslouge <53574879+nicolaslouge@users.noreply.github.com>
2024-09-22 22:22:10 -0400

    Completed ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(valueA, valueB) {
+  return valueA + valueB;
 };
 
-const subtract = function() {
-	
+const subtract = function(valueA, valueB) {
+	return valueA - valueB;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(valueA, valueB) {
+	return Math.pow(valueA, valueB);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
